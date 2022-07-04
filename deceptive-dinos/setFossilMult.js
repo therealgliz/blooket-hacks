55e8824e0ec10768c43fe496c469b2b85eb522ea
qaiik <82181231+qaiik@users.noreply.github.com>
2022-07-03 18:37:33 -0600

    Update setFossilMult.js

--- deceptive-dinos/setFossilMult.js
+++ deceptive-dinos/setFossilMult.js
@@ -1,12 +1,51 @@
 (async ()=>{
   let i = document.createElement("iframe");
-  let prompt = i.contentWindow.bind(window);
-  let alert = i.contentWindow.bind(window)
+  document.body.appendChild(i)
+  let prompt = i.contentWindow.prompt.bind(window);
+  let alert = i.contentWindow.alert.bind(window)
+  let confirm = i.contentWindow.alert.bind(window);
+  i.remove();
+  
   try {
     Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.setState({
-      fossilMult: 1e9
+      fossilMult: Number(prompt("Fossil multiplier: "))
     })
   } catch (e) {
     if (confirm("An error occured. Would you like to join the discord server?")) {
+      window.open("https://snak3s773.github.io/blooket-hack/discord.html")
     }
+  }
 })();
+
+function footer() {
+    let element = document.createElement('div');
+
+    element.style = `font-family: "Nunito", sans-serif; font-size: 14px; height: 65px; width: 175px; border: 4px solid rgb(15, 15, 15); background: rgb(240, 240, 240); position: absolute; top: 20x; left: 20px; border-radius: 10px; color: rgb(0, 0, 0); text-align: center;`;
+    element.innerHTML = `<p>Made by gliz <br> My <a style="color: #0000ff;" href="https://twitter.com/glizuwu" target="_blank">twitter</a></p>`;
+    document.body.appendChild(element);
+
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    element.onmousedown = ((e = window.event) => {
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = (() => {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        });
+        document.onmousemove = ((e) => {
+            e = e || window.event;
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            let top = (element.offsetTop - pos2) > 0 ? (element.offsetTop - pos2) : 0;
+            let left = (element.offsetLeft - pos1) > 0 ? (element.offsetLeft - pos1) : 0;
+            element.style.top = top + "px";
+            element.style.left = left + "px";
+        });
+    });
+};
+
+footer();

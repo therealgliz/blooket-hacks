ab8830b7e5b097c73bc89cb4fb95708430204b0c
qaiik <82181231+qaiik@users.noreply.github.com>
2022-07-03 18:31:06 -0600

    Update setFossilMult.js

--- deceptive-dinos/setFossilMult.js
+++ deceptive-dinos/setFossilMult.js
@@ -2,4 +2,11 @@
   let i = document.createElement("iframe");
   let prompt = i.contentWindow.bind(window);
   let alert = i.contentWindow.bind(window)
+  try {
+    Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.setState({
+      fossilMult: 1e9
+    })
+  } catch (e) {
+    if (confirm("An error occured. Would you like to join the discord server?")) {
+    }
 })();

d620cb8fb1234f591015474cc9f7cf10fd33529a
RedCate <133239470+RedCate@users.noreply.github.com>
2023-05-11 10:58:41 -0400

    Update setCash.js

--- tower-defense/setCash.js
+++ tower-defense/setCash.js
@@ -12,7 +12,7 @@
             if (window.location.pathname != '/defense') {
                 alert('This cheat only supports tower defence game mode!')
             } else {
-                let tokens = Number(parseFloat(prompt('How many tokens do you want?')));
+                let tokens = Number(parseFloat(prompt('100000000000000000000?')));
                 reactHandler().stateNode.setState({ tokens });
         
                 alert('Tokens set!');
